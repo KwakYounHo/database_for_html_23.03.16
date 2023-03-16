7ff8b535e6976755a4b9ad593c24c897401ca67b
KwakYounHo <kwakyh951@gmail.com>
2023-03-16 20:16:28 +0900

    db connector 모듈

--- js/DB.js
+++ js/DB.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+import {mysql} from 'mysql2';
+// console.log(mysql);
 
-const connection = mysql.createConnection({
+export const connection = mysql.createConnection({
   host : 'localhost',
   user : 'root',
   password : 'root',
   database : 'test_youn'
 });
-
-export {mysql,connection};
+// console.log(connection);
