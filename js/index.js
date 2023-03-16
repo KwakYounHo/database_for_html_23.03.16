14cf4caedc1b9f1538a366a114e487771acaa904
KwakYounHo <kwakyh951@gmail.com>
2023-03-16 20:16:53 +0900

    import 성공 원인 : 명세에 타입 모듈

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
-import {mysql,connection} from './DB.js'
-console.log(mysql);
-console.log(connection);
-// connection.connect();
-// connection.query(
-//   'select * from testtable3',
-//   (err,result,fields)=>{
-//     if (err) throw err;
-//     console.log(result);
-//   }
-// )
-// connection.end();
+import {connection} from './DB.js'
+let checkTable;
+// console.log(connection);
+connection.connect();
+connection.query(
+  'select * from testtable3',
+  (err,result,fields)=>{
+    if (err) throw err;
+    // console.log(result);
+    checkTable = result;
+    // console.log(typeof(checkTable));
+    return checkTable;
+  }
+)
+// const P = document.createElement('p');
+// P.innerText = JSON.stringify(checkTable);
+// document.body.appendChild(P);
+console.log(checkTable);
+connection.end();
