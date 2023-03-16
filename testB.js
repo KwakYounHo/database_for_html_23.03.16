215468ef1ab668c2e1c65631b7cd1f289aeb13ae
KwakYounHo <kwakyh951@gmail.com>
2023-03-16 20:15:53 +0900

    전역변수 리턴이 왜 안돼

--- testB.js
+++ testB.js
@@ -1,2 +1,10 @@
 import {a} from './testA.js';
-console.log(a);
+// console.log(typeof(a));
+let testabc;
+function letsGoJSON() {
+    testabc=a;
+    // console.log(testabc);
+    return testabc;
+  }
+letsGoJSON()
+console.log(testabc);
