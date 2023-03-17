cb481e2f6af5e7cfa77ba76c67db81d173f924a3
KwakYounHo <kwakyh951@gmail.com>
2023-03-17 09:22:58 +0900

    export에 default 있고 없고 차이

---
+++ defaultTestExport.js
@@ -0,0 +1,9 @@
+export default {
+  'test':'funny',
+  'abc':'defu',
+  '가나다':'라마바'
+}
+
+export const User = { // export 옆에 'default'를 추가해보았습니다.
+  'Hi':'Hellow',
+}
---
+++ defaultTestImport.js
@@ -0,0 +1,5 @@
+import CDF from './defaultTestExport.js'
+console.log(CDF);
+
+import {User} from './defaultTestExport.js'
+console.log(User);
