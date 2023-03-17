78c95fd29c1b6ee2b34c4c02f353a91770dd9d41
KwakYounHo <kwakyh951@gmail.com>
2023-03-17 10:53:56 +0900

    전역변수에 저장이 되지 않는 문제 탐색중

--- js/index.js
+++ js/index.js
@@ -1,19 +1,23 @@
+// const mysql = require('mysql2');
 import {connection} from './DB.js'
 let checkTable;
 // console.log(connection);
 connection.connect();
+// const why =
 connection.query(
   'select * from testtable3',
   (err,result,fields)=>{
-    if (err) throw err;
-    // console.log(result);
-    checkTable = result;
-    // console.log(typeof(checkTable));
-    return checkTable;
-  }
-)
-// const P = document.createElement('p');
-// P.innerText = JSON.stringify(checkTable);
-// document.body.appendChild(P);
-console.log(checkTable);
-connection.end();
+      if (err) throw err;
+      checkTable = result;
+      // console.log(result);
+      console.log(checkTable);
+      // console.log(typeof(checkTable));
+      return checkTable;
+    }
+  )
+  // const P = document.createElement('p');
+  // P.innerText = JSON.stringify(checkTable);
+  // document.body.appendChild(P);
+  connection.end();
+  // why();
+  console.log(checkTable);
